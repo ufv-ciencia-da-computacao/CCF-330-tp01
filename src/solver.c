6cef19117521d3a4d44dc71a093fdc2b2f94366c
Otávio S. Gomes <otaviosantosg@gmail.com>
2021-03-30 18:50:35 -0300

    melhorias de percurso

--- src/solver.c
+++ src/solver.c
@@ -1,58 +1,66 @@
 #include "solver.h"
 
-int move_student_util(tuple_t pos, int ***visited, int **matrix, int keys, int max_lin, int max_col, int iter) {
+int move_student_util(tuple_t pos, int ***visited, int **matrix, int *keys, int max_lin, int max_col, int iter, int *fim) {
     if (pos.x == 0) {
         (*visited)[pos.x][pos.y]=iter;
+        *fim = 1;
+        printf("\nKEYS: %d\n\n", *keys);
         return 1;
     }
+
+    if(*fim == 1) return 1;
     
-    if (matrix[pos.x][pos.y] == 2 || matrix[pos.x][pos.y] == 3 && keys <= 0) {
+    if (matrix[pos.x][pos.y] == 2 || matrix[pos.x][pos.y] == 3 && *keys <= 0) {
         return 0;
     } else if (matrix[pos.x][pos.y] == 3) {
-        keys--; //mudar esse requisito do keys
+        (*keys)--; //mudar esse requisito do keys
     }
 
     if ((*visited)[pos.x][pos.y]!=0) {
         return 0;
     } else {
         (*visited)[pos.x][pos.y] = iter;
-        if (pos.y + 1 <= max_col) {
+    
+        if (pos.x - 1 >= 0) {
             tuple_t new_pos;
-            new_pos.x = pos.x;
-            new_pos.y = pos.y+1;
-            if (move_student_util(new_pos, visited, matrix, keys, max_lin, max_col, iter+1)) return 1;
-        } 
+            new_pos.x = pos.x-1;
+            new_pos.y = pos.y;
+            move_student_util(new_pos, visited, matrix, keys, max_lin, max_col, iter+1, fim);
+        }
 
         if (pos.y - 1 >= 0) {
             tuple_t new_pos;
             new_pos.x = pos.x;
             new_pos.y = pos.y-1;
-            if (move_student_util(new_pos, visited, matrix, keys, max_lin, max_col, iter+1)) return 1;
+            move_student_util(new_pos, visited, matrix, keys, max_lin, max_col, iter+1, fim);
+        }
+
+        if (pos.y + 1 <= max_col) {
+            tuple_t new_pos;
+            new_pos.x = pos.x;
+            new_pos.y = pos.y+1;
+            move_student_util(new_pos, visited, matrix, keys, max_lin, max_col, iter+1, fim);
         }
 
         if (pos.x + 1 <= max_lin) {
             tuple_t new_pos;
             new_pos.x = pos.x+1;
             new_pos.y = pos.y;
-            if (move_student_util(new_pos, visited, matrix, keys, max_lin, max_col, iter+1)) return 1;
+            move_student_util(new_pos, visited, matrix, keys, max_lin, max_col, iter+1, fim);
         }
-        
-        if (pos.x - 1 >= 0) {
-            tuple_t new_pos;
-            new_pos.x = pos.x-1;
-            new_pos.y = pos.y;
-            if (move_student_util(new_pos, visited, matrix, keys, max_lin, max_col, iter+1)) return 1;
-        }    
+          
     }
 }
 
 void solve(int ***visited, int **matrix, int lin, int col, int keys) {
     tuple_t pos = find_initial_position(matrix, lin, col);
+    int fim = 0;
     
-    if (move_student_util(pos, visited, matrix, keys, lin-1, col-1, 1)) {
+    if (move_student_util(pos, visited, matrix, &keys, lin-1, col-1, 1, &fim)) {
         for (int i = 0; i < lin; i++) {
             for (int j = 0; j < col; j++) {
-                printf("%d", (*visited)[i][j]);
+                if((*visited)[i][j]>9) printf("%d ", (*visited)[i][j]);
+                else                   printf("%d  ", (*visited)[i][j]);
             }
             printf("\n");
         }
@@ -62,7 +70,6 @@ void solve(int ***visited, int **matrix, int lin, int col, int keys) {
     
     
 }
-
 
 tuple_t find_initial_position(int **matrix, int lin, int col) {
     tuple_t initial_position;
